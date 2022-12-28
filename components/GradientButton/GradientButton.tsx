f7afc1a6a1eb668e5ad3e4dbde542c4a14153852
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-28 16:41:15 +0000

    Added HTMLButtonElement type

--- components/GradientButton/GradientButton.tsx
+++ components/GradientButton/GradientButton.tsx
@@ -1,11 +1,19 @@
-export function GradientButton() {
+import React from "react";
+
+type GradientButtonTYPE = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export const GradientButton: React.FC<GradientButtonTYPE> = (props) => {
   return (
-    <div>
-      <button className="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group blue-gradient group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white">
-        <span className="px-20 py-3 transition-all ease-in duration-100 bg-white rounded-md group-hover:bg-opacity-0">
-          Get Started
-        </span>
-      </button>
-    </div>
+    <button
+      {...props}
+      className="inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group blue-gradient group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white"
+    >
+      <span className="px-20 py-3 transition-all duration-100 ease-in bg-white rounded-md group-hover:bg-opacity-0">
+        Get Started
+      </span>
+    </button>
   );
-}
+};
