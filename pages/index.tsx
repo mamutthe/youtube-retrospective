3651b6817c179b963376b39a42dc7a0680e46c15
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-28 17:41:28 +0000

    Added handleRedirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { AnimatedTitle, Paragraph, GradientButton } from '../components';
+import { AnimatedTitle, Paragraph, GradientButton } from "../components";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const handleRedirect = () => {
+    router.push("/HistoryAnalyzer");
+  };
+
   return (
-    <section className="p-2 flex items-center flex-col space-y-10">
+    <section className="flex flex-col items-center p-2 space-y-10">
       <AnimatedTitle />
       <Paragraph />
-      <GradientButton />
+      <GradientButton onClick={handleRedirect} />
     </section>
   );
 }
