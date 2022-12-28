56fdd51625af37c2670e90e4be855638937b1d07
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-28 18:15:39 +0000

    Changed styles and added localStorage info

--- components/StoryCards/MostWatchedChannelCard/MostWatchedChannelCard.tsx
+++ components/StoryCards/MostWatchedChannelCard/MostWatchedChannelCard.tsx
@@ -2,22 +2,30 @@ import ytChannelPic from "/workspaces/yt-history-stats/public/picExample.jpg";
 import Image from "next/image";
 import { BaseCard } from "../BaseCard/BaseCard";
 import { RoundedTransparentCard } from "../../RoundedTransparentCard/RoundedTransparentCard";
+import { useEffect, useState } from "react";
+import { channelsWithCountTYPE } from "../../../types/types";
 
 export function MostWatchedChannelCard() {
-  const mostWatchedChannel = "Web Diva Tulla Luana";
-  const channelLink = "https://www.youtube.com/@PQPWEBDIVATULLALUANA;";
-  const mostWatchedChannelTime = 150;
+  const [mostWatchedChannel, setMostWatchedChannel] =
+    useState<channelsWithCountTYPE | null>(null);
+
+  useEffect(() => {
+    if (window.localStorage.getItem("topChannels") === null) return;
+    setMostWatchedChannel(
+      JSON.parse(window.localStorage.getItem("topChannels") as string)[0]
+    );
+  }, []);
 
   return (
-    <BaseCard className="flex flex-col items-center justify-center w-full h-full px-4 space-y-4 bg-gradient-to-r from-indigo-500 to-violet-600 rounded-2xl">
-      <p className="text-2xl font-medium text-slate-200 mt-7">
+    <BaseCard className="flex flex-col items-center w-full h-full px-4 space-y-4 bg-gradient-to-r from-indigo-500 to-violet-700 rounded-2xl">
+      <p className="text-2xl font-medium text-white mt-7">
         Your most watched channel was
       </p>
 
       <div>
         <a
           className="flex flex-col items-center"
-          href={channelLink}
+          href={mostWatchedChannel?.channelTitleUrl}
           target="_blank"
           rel="noreferrer"
         >
@@ -29,18 +37,19 @@ export function MostWatchedChannelCard() {
             height={180}
           ></Image>
 
-          <p className="text-slate-200 font-bold text-[2.2rem]">
-            {mostWatchedChannel}
+          <p className="text-white font-bold text-[2.2rem]">
+            {mostWatchedChannel?.channelTitle}
           </p>
         </a>
       </div>
 
-      <RoundedTransparentCard className="flex-col py-2 mt-10 space-y-1 bg-white/70 rounded-3xl">
-        <span className="p-2 text-3xl font-medium text-center text-slate-800">
-          You watched it for
-        </span>
+      <span className="p-2 text-xl font-bold text-center text-white">
+        {`That's how many diffrent videos you watched from them:`}
+      </span>
+
+      <RoundedTransparentCard className="flex-col py-2 mt-6 space-y-1 bg-white/70 backdrop-saturate-200 rounded-3xl">
         <span className="p-2 font-extrabold text-center text-slate-800 text-7xl">
-          {mostWatchedChannelTime} minutes
+          {`${mostWatchedChannel?.count} videos`}
         </span>
       </RoundedTransparentCard>
     </BaseCard>
