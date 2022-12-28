c7f3539b4f76c768d7731f6b4381110cac62ff73
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-28 15:55:12 +0000

    Fixed local storage loading

--- components/StoryCards/TopChannelsCard/TopChannelsCard.tsx
+++ components/StoryCards/TopChannelsCard/TopChannelsCard.tsx
@@ -1,11 +1,22 @@
-import { BaseCard } from '../BaseCard/BaseCard';
-import { RoundedTransparentCard } from '../../RoundedTransparentCard/RoundedTransparentCard';
-import ytChannelPic from '/workspaces/yt-history-stats/public/picExample.jpg';
-import Image from 'next/image';
-
-const topChannels = window.localStorage.getItem('topChannels');
+import { BaseCard } from "../BaseCard/BaseCard";
+import { RoundedTransparentCard } from "../../RoundedTransparentCard/RoundedTransparentCard";
+import ytChannelPic from "/workspaces/yt-history-stats/public/picExample.jpg";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { topChannelsTYPE } from "../../../types/types";
 
 export function TopChannelsCard() {
+  const [topChannels, setTopChannels] = useState<topChannelsTYPE[]>(
+    [] as topChannelsTYPE[]
+  );
+
+  useEffect(() => {
+    if (window.localStorage.getItem("topChannels") === null) return;
+    setTopChannels(
+      JSON.parse(window.localStorage.getItem("topChannels") as string)
+    );
+  }, []);
+
   return (
     <>
       <BaseCard className="grid w-full h-full p-1 pt-6 text-sm bg-gradient-to-r from-amber-500 to-orange-500 bg-orange bg-size grid-row-3">
@@ -21,27 +32,27 @@ export function TopChannelsCard() {
           ></Image>
           <RoundedTransparentCard className="p-4 h-16 w-[21.5rem]">
             <span className="text-3xl font-bold text-orange-500">
-              {mostWatchedChannelsList[0].channel}
+              {topChannels[0]?.channelTitle}
             </span>
           </RoundedTransparentCard>
         </div>
         <div className="grid grid-cols-2 grid-rows-2 gap-4 p-4 mb-4">
-          {mostWatchedChannelsList.slice(1).map((channel, index) => {
+          {topChannels.slice(1).map((channel, index) => {
             if (index === 0) {
               return (
                 <RoundedTransparentCard
-                  key={channel.channel}
+                  key={channel.channelTitle}
                   className="col-span-2 h-14"
                 >
-                  <p className="text-xl font-medium text-center text-orange-500">{`2º ${channel.channel}`}</p>
+                  <p className="text-xl font-medium text-center text-orange-500">{`2º ${channel.channelTitle}`}</p>
                 </RoundedTransparentCard>
               );
             }
             return (
-              <RoundedTransparentCard key={channel.channel} className="h-14">
+              <RoundedTransparentCard key={channel.channelTitle} className="h-14">
                 <span className="text-lg font-medium text-center text-orange-500">{`${
                   index + 2
-                }º ${channel.channel}`}</span>
+                }º ${channel.channelTitle}`}</span>
               </RoundedTransparentCard>
             );
           })}
