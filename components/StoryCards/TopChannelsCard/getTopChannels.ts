d6764eb9b97bdde2bbad569775a7e8c24b722a2c
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-29 04:18:14 +0000

    Import formatting

--- components/StoryCards/TopChannelsCard/getTopChannels.ts
+++ components/StoryCards/TopChannelsCard/getTopChannels.ts
@@ -1,6 +1,8 @@
-import { reducedHistoryTYPE, reducedYTVideoTYPE, topChannelsTYPE } from "../../../types/types";
-
-
+import {
+  reducedHistoryTYPE,
+  reducedYTVideoTYPE,
+  topChannelsTYPE,
+} from "../../../types/types";
 
 export const getTopChannels = (reducedHistory: reducedHistoryTYPE) => {
   //Gera um objeto que contém a quantidade de videos diferentes de cada canal no histórico
@@ -18,15 +20,15 @@ export const getTopChannels = (reducedHistory: reducedHistoryTYPE) => {
       };
     }
   });
-  
+
   //Ordena a lista pelo número de videos assistidos de cada canal
-    const sortedTopChannels = Object.values(topChannels).sort(
+  const sortedTopChannels = Object.values(topChannels).sort(
     (a: any, b: any) => b.count - a.count
-    );
+  );
   sortedTopChannels.length = 10;
-  
+
   //Salva a lista dos canais mais vistos no localStorage
   window.localStorage.setItem("topChannels", JSON.stringify(sortedTopChannels));
-  
-return sortedTopChannels;
+
+  return sortedTopChannels;
 };
