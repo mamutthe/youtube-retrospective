d8e46aa5ee246479faa2c47361484ffab4c009d8
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-29 04:18:43 +0000

    Inserted real data to the component

--- components/StoryCards/MostListenedSongsCard/MostListenedSongsCard.tsx
+++ components/StoryCards/MostListenedSongsCard/MostListenedSongsCard.tsx
@@ -1,63 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BaseCard } from "../BaseCard/BaseCard";
 import { RoundedTransparentCard } from "../../RoundedTransparentCard/RoundedTransparentCard";
 import Image from "next/image";
 import ytChannelPic from "/workspaces/yt-history-stats/public/picExample.jpg";
+import { reducedHistoryTYPE, reducedYTVideoTYPE } from "../../../types/types";
+import Link from "next/link";
 
 export function MostListenedSongsCard() {
-  const MostListenedSongs = [
-    {
-      title: "Gloria",
-      link: "https://www.youtube.com/watch?v=1",
-      songCover: ytChannelPic,
-    },
-    {
-      title: "Bad Romance",
-      link: "https://www.youtube.com/watch?v=GlgtTmuZRiM&t=351s",
-      songCover: ytChannelPic,
-    },
-    {
-      title: "Physical",
-      link: "https://www.youtube.com/watch?v=1",
-      songCover: ytChannelPic,
-    },
-    {
-      title: "Dancing Queen",
-      link: "https://www.youtube.com/watch?v=1",
-      songCover: ytChannelPic,
-    },
-    {
-      title: "I Want It That Way",
-      link: "https://www.youtube.com/watch?v=1",
-      songCover: ytChannelPic,
-    },
-  ];
+  const [mostListenedSongs, setMostListenedSongs] =
+    useState<reducedHistoryTYPE>([] as reducedHistoryTYPE);
+
+  useEffect(() => {
+    if (window.localStorage.getItem("mostListenedSongs") === null) {
+      throw new Error("mostListenedSongs: no data found in localStorage");
+    }
+    setMostListenedSongs(
+      JSON.parse(window.localStorage.getItem("mostListenedSongs") as string)
+    );
+  }, []);
+
   return (
     <BaseCard className="flex flex-col items-center justify-center px-4 py-1 space-y-4 bg-gradient-to-r from-pink-500 to-rose-700">
-      <span className="mb-4 text-2xl font-medium text-slate-200">
-        Most Listened Songs
+      <span className="mb-4 text-2xl font-medium text-white">
+        Your most listened songs were
       </span>
-      {MostListenedSongs.map((video) => (
-        <a
+      {mostListenedSongs.map((video) => (
+        <Link
           className="w-full"
           key={video.title}
-          href={video.link}
+          href={video.titleUrl}
           target="_blank"
           rel="noreferrer"
         >
           <RoundedTransparentCard className="flex h-[5.5rem] w-full rounded-2xl">
             <Image
               className="h-full mr-auto rounded-l-2xl"
-              src={video.songCover}
               alt="channel pic"
+              src={ytChannelPic}
               width={0}
               height={0}
             />
-            <span className="mr-auto text-xl font-medium text-center text-rose-900 trucate">
+            <span className="mr-auto text-xl font-medium text-center text-teal-900 trucate">
               {video.title}
             </span>
           </RoundedTransparentCard>
-        </a>
+        </Link>
       ))}
     </BaseCard>
   );
