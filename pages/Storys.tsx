2cb237b85db1d2c1149eba5aa6866690eb406ee4
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-30 01:12:41 +0000

    Added TopArtistsCard and TopMonthCard

--- pages/Storys.tsx
+++ pages/Storys.tsx
@@ -7,6 +7,8 @@ export default function Storys() {
       <MostWatchedChannelCard />
       <MostWatchedVideosCard />
       <MostListenedSongsCard />
+      <TopArtistsCard />
+      <TopMonthCard />
     </>
   );
 }
