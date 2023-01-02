2f592f123c42baf536053dad630fbc2c8db05a34
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2023-01-02 00:13:50 +0000

    Updated HistoryAnalyzer

--- pages/HistoryAnalyzer.tsx
+++ pages/HistoryAnalyzer.tsx
@@ -1,39 +1,60 @@
 import React, { useState, createContext, useEffect } from "react";
-import { useRouter } from "next/router";
 import { historyTYPE, reducedHistoryTYPE } from "../types/types";
+import { GradientButton } from "../components";
 import { HistoryUpload } from "../components";
-import { getTopChannelsAndArtists } from "../history-analyzer/getTopChannelsAndArtists";
-import { getTopVideosAndSongs } from "../history-analyzer/getTopVideosAndSongs";
+import { StatsExplorer } from "../components";
+import { getTopChannels } from "../history-analyzer/getTopChannels";
+import { getTopVideos } from "../history-analyzer/getTopVideos";
+import { getVideosWatchedPerMonth } from "../history-analyzer/getFeaturedStats";
+import { getAmountOfVideosWatched } from "../history-analyzer/getFeaturedStats";
+import { getTopVideosPerMonth } from "../history-analyzer/getTopVideosPerMonth";
 
 export const HistoryContext = createContext({
+  reducedHistory: [] as reducedHistoryTYPE,
+  setHistory: (history: historyTYPE) => {},
   setReducedHistory: (reducedHistory: reducedHistoryTYPE) => {},
   handleGenerate: () => {},
+  handleExplore: () => {},
 });
 
 const HistoryAnalyzer: React.FC = () => {
-  const router = useRouter();
+  //Estado referente ao histórico upado pelo usuário
+  const [history, setHistory] = useState<historyTYPE>([] as historyTYPE);
 
   //Estado referente ao histórico upado pelo usuário e ao histórico reduzido
-  const [reducedHistory, setReducedHistory] =
-    useState<reducedHistoryTYPE | null>(null);
+  const [reducedHistory, setReducedHistory] = useState<reducedHistoryTYPE>(
+    [] as reducedHistoryTYPE
+  );
+
+  const [isExplore, setIsExplore] = useState(false);
 
   const handleGenerate = () => {
-    getTopChannelsAndArtists(reducedHistory as reducedHistoryTYPE, "YouTube");
-    getTopChannelsAndArtists(reducedHistory as reducedHistoryTYPE, "YouTube Music");
-    getTopVideosAndSongs(reducedHistory as reducedHistoryTYPE, 5, "YouTube");
-    getTopVideosAndSongs(
-      reducedHistory as reducedHistoryTYPE,
-      5,
-      "YouTube Music"
-    );
-    router.push("/Storys");
+    getTopVideos(reducedHistory as reducedHistoryTYPE, 6);
+
+    getTopVideosPerMonth(reducedHistory as reducedHistoryTYPE);
+
+    getVideosWatchedPerMonth(history as historyTYPE);
+
+    getAmountOfVideosWatched(history as historyTYPE);
+
+    getTopChannels(reducedHistory as reducedHistoryTYPE);
+  };
+
+  const handleExplore = () => {
+    setIsExplore(true);
   };
 
   return (
-    <HistoryContext.Provider value={{ setReducedHistory, handleGenerate }}>
-      <div>
-        <HistoryUpload />
-      </div>
+    <HistoryContext.Provider
+      value={{
+        reducedHistory,
+        setHistory,
+        setReducedHistory,
+        handleGenerate,
+        handleExplore,
+      }}
+    >
+      <div>{isExplore ? <StatsExplorer /> : <HistoryUpload />}</div>
     </HistoryContext.Provider>
   );
 };
